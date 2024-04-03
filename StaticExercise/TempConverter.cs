a146a81f1fe2caa35dbbd79319dc39fe675c23bc
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-04-03 13:51:38 -0500

    Adjusted the class system and completed exercise

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+    }
+}
