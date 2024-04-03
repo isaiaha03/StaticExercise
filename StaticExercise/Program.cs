96e706341eb55e5650fc9eb7b89e51f2f4322c32
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-04-03 13:41:11 -0500

    Completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace StaticExercise
 {
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9/5) + 32;
+            return fahrenheit;
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double fahrenheit = 90;
+            double celsius = 10;
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(fahrenheit));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(celsius));
         }
     }
 }
